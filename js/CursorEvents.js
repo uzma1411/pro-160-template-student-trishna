18ceaf9cf45675a11cec414495dbc6139bcb396a
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-04-16 07:28:29 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" }
+  },
+  init: function() {
+    this.handleClickEvents();
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+  },
+  handleClickEvents: function() {
+    //  Click Events
+    this.el.addEventListener("click", evt => {
+      const placesContainer = document.querySelector("#places-container");
+      const { state } = placesContainer.getAttribute("tour");
+
+      if (state === "places-list") {
+        const id = this.el.getAttribute("id");
+        const placesId = ["place-home", "place-garden", "place-main_gate"];
+        if (placesId.includes(id)) {
+          placesContainer.setAttribute("tour", {
+            state: "view",
+            selectedPlace: id
+          });
+        }
+      }
+    });
+  },
+  handleMouseEnterEvents: function() {
+    // Mouse Center Events
+    
+  },
+  handlePlacesListState: function() {
+   
+  },
+  handleMouseLeaveEvents: function() {
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", () => {
+      const placesContainer = document.querySelector("#places-container");
+      const { state } = placesContainer.getAttribute("tour");
+      if (state === "places-list") {
+        const { selectedItemId } = this.data;
+        if (selectedItemId) {
+          const el = document.querySelector(`#${selectedItemId}`);
+          const id = el.getAttribute("id");
+          if (id == selectedItemId) {
+            el.setAttribute("material", {
+              opacity: 0.4
+            });
+          }
+        }
+      }
+    });
+  }
+});
